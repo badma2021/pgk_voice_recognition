7597f29468654d70465b9626acbb2ff5cf942ddf
badma <badma.chedyrov@yahoo.com>
2023-01-02 17:13:35 +0300

    add controller advice and exception handler

---
+++ src/main/java/com/example/pgk/exception/DefaultAdvice.java
@@ -0,0 +1,16 @@
+package com.example.pgk.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class DefaultAdvice {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Response> handleException(UserNotFoundException e) {
+        Response response = new Response(e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
