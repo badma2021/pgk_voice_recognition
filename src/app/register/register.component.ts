59edf08398a5b0348fc56e3ae7bb234c848771f0
badma <badma.chedyrov@yahoo.com>
2023-01-21 20:05:01 +0300

    1st commit

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,7 +8,8 @@ import { AuthService } from '../_services/auth.service';
 })
 export class RegisterComponent implements OnInit {
   form: any = {
-    username: null,
+    firstName: null,
+    lastName: null,
     email: null,
     password: null
   };
@@ -22,9 +23,9 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): void {
-    const { username, email, password } = this.form;
+    const { firstName, lastName, email, password } = this.form;
 
-    this.authService.register(username, email, password).subscribe(
+    this.authService.register(firstName, lastName, email, password).subscribe(
       data => {
         console.log(data);
         this.isSuccessful = true;
