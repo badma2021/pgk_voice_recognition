d5d460f327db800d17ae59cf3ce0273c13f37455
badma <badma.chedyrov@yahoo.com>
2023-01-22 23:36:28 +0300

    insert record

---
+++ src/app/record/record.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { RecordService } from '../_services/record.service';
+
+@Component({
+  selector: 'app-record',
+  templateUrl: './record.component.html',
+  styleUrls: ['./record.component.css']
+})
+export class RecordComponent implements OnInit{
+ form: any = {
+       createdAt: null,
+       expenseTitleId: null,
+       amount: null,
+       comments: null,
+       userId: null,
+       currencyName: null,
+       exchangeRateToRuble: null
+  };
+  errorMessage = '';
+
+  constructor(private recordService: RecordService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const { createdAt, expenseTitleId, amount, comments, userId, currencyName, exchangeRateToRuble } = this.form;
+  }
+}
