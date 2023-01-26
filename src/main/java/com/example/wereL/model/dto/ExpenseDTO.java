5f4985d9052fc48cef4baa923834b8507c20fb0d
badma <badma.chedyrov@yahoo.com>
2023-01-27 01:43:21 +0300

    json array for one element issue is resolved

--- src/main/java/com/example/wereL/model/dto/ExpenseDTO.java
+++ src/main/java/com/example/wereL/model/dto/ExpenseDTO.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @ToString
-public class ExpenseDTO {
+public class ExpenseDTO{
     private Long id;
     private LocalDateTime createdAt;
     private int expenseTitleId;
