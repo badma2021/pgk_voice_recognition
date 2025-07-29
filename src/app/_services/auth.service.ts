649460724dd83f41633dbaf3dfffaa69ff0fbffc
badma <badma.chedyrov@yahoo.com>
2025-07-29 12:21:31 +0300

    new dns

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 //const AUTH_API = 'http://18.195.42.80:8888/api/v1/';
-const AUTH_API = 'http://5.144.181.199:8888/api/v1/';
+const AUTH_API = 'http://89.213.52.246:8888/api/v1/';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
